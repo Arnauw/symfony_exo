d778aca38cfa281d60020c32879c0fea07f51d4f
Arnauw <arnaud.rabel@gmail.com>
2025-07-21 16:19:14 +0200

    test commit from PHPStorm

--- src/Controller/HomePageController.php
+++ src/Controller/HomePageController.php
@@ -5,6 +5,8 @@ namespace App\Controller;
 use App\Entity\Crud;
 use App\Form\CrudType;
 
+use App\Repository\CrudRepository;
+use Doctrine\DBAL\Exception;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
@@ -13,15 +15,23 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 final class HomePageController extends AbstractController
 {
-    #[Route('/', name: 'app_home_page')]
-    public function homepage(): Response
+    /**
+     * @throws Exception
+     */
+    #[Route('/', name: 'home_page')]
+    public function homepage(CrudRepository $crudRepo): Response
     {
+
+        $datas = $crudRepo->findAll();
+
+
         return $this->render('home_page/index.html.twig', [
             'controller_name' => 'HomePageController',
+            'datas' => $datas
         ]);
     }
 
-    #[Route('/create', name: 'app_create_form')]
+    #[Route('/create', name: 'create_form')]
     public function create(Request $request, EntityManagerInterface $em): Response
     {
         /**
@@ -37,11 +47,56 @@ final class HomePageController extends AbstractController
             $em->flush();
 
             $this->addFlash('notice', 'Successfully created');
-            return $this->redirectToRoute('app_home_page');
+            return $this->redirectToRoute('home_page');
         }
 
         return $this->render('form/createForm.html.twig', [
             'form' => $form->createView()
         ]);
     }
+
+    #[Route('/update/{id}', name: 'update')]
+    public function update(Request $request, CrudRepository $crudRepo, EntityManagerInterface $em, $id): Response
+    {
+
+        $data = $crudRepo->find($id);
+        $form = $this->createForm(CrudType::class, $data);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+
+            $em->persist($data);
+            $em->flush();
+
+            $this->addFlash('notice', 'Successfully created');
+            return $this->redirectToRoute('home_page');
+        }
+
+        return $this->render('home_page/index.html.twig', [
+            'data' => $data
+        ]);
+    }
+
+    #[Route('/delete/{id}', name: 'delete')]
+    public function delete(Request $request, CrudRepository $crudRepo, EntityManagerInterface $em, $id): Response
+    {
+
+        $data = $crudRepo->find($id);
+        $form = $this->createForm(CrudType::class, $data);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+
+            $em->remove($data);
+            $em->flush();
+
+            $this->addFlash('notice', 'Successfully created');
+            return $this->redirectToRoute('home_page');
+        }
+
+        return $this->render('home_page/index.html.twig', [
+            'data' => $data
+        ]);
+    }
+
 }
