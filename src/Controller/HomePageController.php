b5ed7c00a7a3595b882b8b426b5324731a4b08b9
Arnauw <arnaud.rabel@gmail.com>
2025-07-21 14:44:11 +0200

    test commit from PHPStorm

--- src/Controller/HomePageController.php
+++ src/Controller/HomePageController.php
@@ -2,9 +2,14 @@
 
 namespace App\Controller;
 
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use App\Entity\Crud;
+use App\Form\CrudType;
+
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 final class HomePageController extends AbstractController
 {
@@ -13,6 +18,30 @@ final class HomePageController extends AbstractController
     {
         return $this->render('home_page/index.html.twig', [
             'controller_name' => 'HomePageController',
+        ]);
+    }
+
+    #[Route('/create', name: 'app_create_form')]
+    public function create(Request $request, EntityManagerInterface $em): Response
+    {
+        /**
+         *
+         */
+        $crud = new Crud();
+        $form = $this->createForm(CrudType::class, $crud);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+
+            $em->persist($crud);
+            $em->flush();
+
+            $this->addFlash('notice', 'Successfully created');
+            return $this->redirectToRoute('app_home_page');
+        }
+
+        return $this->render('form/createForm.html.twig', [
+            'form' => $form->createView()
         ]);
     }
 }
